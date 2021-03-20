e607d70590e456522c67856d16630e871839b1a0
Christopher Zell <zelldon91@googlemail.com>
2021-03-20 13:16:49 +0100

    use gcloud authenticator
    gives us
    
    Exception in thread main java.lang.RuntimeException: java.io.IOException: Error getting access token for service account: 400 Bad Request
    POST https://oauth2.googleapis.com/token
    {error:invalid_scope,error_description:Invalid OAuth scope or ID token audience provided.}

--- kubeclient/src/main/kotlin/main.kt
+++ kubeclient/src/main/kotlin/main.kt
@@ -1,19 +1,73 @@
-import io.kubernetes.client.openapi.ApiClient
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.auth.oauth2.ServiceAccountCredentials
 import io.kubernetes.client.openapi.Configuration
 import io.kubernetes.client.openapi.apis.CoreV1Api
 import io.kubernetes.client.util.ClientBuilder
 import io.kubernetes.client.util.KubeConfig
+import io.kubernetes.client.util.authenticators.Authenticator
 import java.io.FileReader
+import java.io.IOException
+import java.time.Instant
+import java.util.*
+
+class ReplacedGCPAuthenticator(val credentials : GoogleCredentials) : Authenticator {
+
+    override fun getName(): String {
+        return "gcp";
+    }
+
+    override fun getToken(config: MutableMap<String, Any>): String {
+        return config["access-token"] as String
+    }
+
+    override fun isExpired(config: MutableMap<String, Any>?): Boolean {
+        val expiryObj = config?.get("expiry")
+        val expiry : Instant;
+        if (expiryObj is Date) {
+            expiry = expiryObj.toInstant();
+        } else if (expiryObj is Instant) {
+            expiry = expiryObj;
+        } else {
+            if (expiryObj !is String) {
+                throw RuntimeException("Unexpected object type: " + expiryObj);
+            }
+            expiry = Instant.parse(expiryObj)
+        }
+        return expiry != null && expiry.compareTo(Instant.now()) <= 0;
+    }
+
+    override fun refresh(config: MutableMap<String, Any>?): MutableMap<String, Any> {
+        try {
+            val accessToken = this.credentials.refreshAccessToken()
+
+            config?.put(ACCESS_TOKEN, accessToken.getTokenValue())
+            config?.put(EXPIRY, accessToken.getExpirationTime())
+        } catch (e : IOException) {
+            throw RuntimeException(e);
+        }
+        return config!!
+    }
+
+    companion object {
+        const val ACCESS_TOKEN = "access-token"
+        const val EXPIRY = "expiry"
+    }
+}
+
+@Throws(IOException::class)
+private fun registerGcloudAuthenticator() {
+    KubeConfig.registerAuthenticator(ReplacedGCPAuthenticator(ServiceAccountCredentials.getApplicationDefault()));
+}
 
 fun main(args: Array<String>) {
 
+    registerGcloudAuthenticator()
     // https://kubernetes.io/docs/tasks/administer-cluster/access-cluster-api/#java-client
     // file path to your KubeConfig
     // only using kube config doesnt work - for gke
     // We get then
     // Unimplemented
     //	at io.kubernetes.client.util.authenticators.GCPAuthenticator.refresh(GCPAuthenticator.java:61)
-
     val kubeConfigPath = System.getenv("HOME") + "/.kube/config";
 
     // loading the out-of-cluster config, a kubeconfig from file-system
